bb094956a4fd5d4494040184061dd7377ba771a1
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-02-23 22:19:04 +0300

    Solution for LeetCode#1732 with the intuition behind it

--- LeetCode#1732.py
+++ LeetCode#1732.py
@@ -1,4 +1,11 @@
 class Solution:
     def largestAltitude(self, gain) -> int:
         # Intuition
-        # 
+        # Find the prefix sum of the gain
+        # return the maximum value from heights
+
+        heights = [0] * (len(gain) + 1)
+
+        for i in range(len(gain)):
+            heights[i + 1] = heights[i] + gain[i]
+        return max(heights)
