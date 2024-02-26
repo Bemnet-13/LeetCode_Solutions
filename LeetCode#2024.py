8cdb4176911933d7d75f49c3a37468cf9605d6da
Bemnet-13 <bemnetaschalewwodaj@gmail.com>
2024-02-26 15:25:18 +0300

    Solved LeetCode#2024 with sliding window technique

--- LeetCode#2024.py
+++ LeetCode#2024.py
@@ -1,37 +1,44 @@
-# Answer v--1.0 
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        # define maximum and count variables t be 0
-        # run a for loop to iterate throiugh each element
-        # define a pointer movl and movr
-        # compare each succedding element with the previous one 
-        # if it is equal then increment both vals by 1
-        # if not decrease both vals by 1 and k by 1 ---until k evals to 0
-        # if k == 0 then maximum = max(maximum, count)
-        # Answer v--1.0 
-        ops = k
-        maximum = 0
-        for i in range(len(answerKey)):
-            answer = answerKey[i]
-            movl = i
-            movr = movl + 1
-            while k >= 0 and movr < len(answerKey):
-                if answerKey[movl] == answerKey[movr]:
-                    movl = movr
-                    movr += 1
-                elif answerKey[movl] != answerKey[movr] and k > 0:
+        # Intuition
+        # Define a function that takes in  the answerKey, the opposing answer,
+        # the coverted answer and k
+        # return the maximum of the two situation
+
+        return max(self.maxConsectutiveWith(answerKey, k, 'T', 'F'), self.maxConsectutiveWith(answerKey, k, 'F', 'T'))
+    
+    def maxConsectutiveWith(self, answerKey, k, corr, opp):
+        start = end = max_len = 0
+
+        while end < len(answerKey):
+            if k < 0:
+                start += 1
+                if answerKey[start - 1] == opp:
+                    k += 1 
+            elif end == len(answerKey) - 1:
+                if answerKey[end] == opp:
                     k -= 1
-                    movl = movr
-                    movr += 1
-                elif answerKey[movl] != answerKey[movr] and k == 0:
+                if k < 0:
+                    max_len = max(max_len, end - start)
                     break
-            
-            maximum = max(maximum, movr-i)
-            k = ops
-        
-        return maximum
+                else:
+                    max_len = max(max_len, end - start + 1)
+                    break
+            elif answerKey[end] == opp:
+                k -= 1
+                if k < 0:
+                    max_len = max(max_len, end - start)
+                    start += 1
+                    if answerKey[start - 1] == opp:
+                        k += 1
+                end += 1
+            else:
+                end += 1
+
+        return max_len
+
 
 
 trial = Solution()
-o = trial.maxConsecutiveAnswers(answerKey = "TTFTTFTT", k = 1)
+o = trial.maxConsecutiveAnswers(answerKey = "TFFT", k = 1)
 print(o)
